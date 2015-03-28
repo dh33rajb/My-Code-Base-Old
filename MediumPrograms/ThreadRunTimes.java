5859b5f9b2d3600f3ed6cb0f0e81580cc0e546d7
Satya Chandu Dheeraj Balakavi <satyacha@buffalo.edu>
2015-03-28 13:44:34 -0400

    Create ThreadRunTimes.java

---
+++ MediumPrograms/ThreadRunTimes.java
@@ -0,0 +1,20 @@
+/*
+Given a list of "threads", which contain 2 variables - starting and ending times - implement a function that will return all running threads at some time t. Optimize it.
+(faster than O(n) ) 
+*/
+
+import java.util.*;
+import java.lang.*;
+
+public class ThreadRunTimes {
+    public static void main (String args[]) {
+        
+    }
+}
+
+class ThreadClass {
+    // Defining variables as public and not using private with getters and setters for sake of simplicity in demonstration
+    public String startTime;
+    public String endTime;
+}
+
