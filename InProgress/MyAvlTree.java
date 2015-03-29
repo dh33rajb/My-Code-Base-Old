548afd3dcb0c73226bb258134d1495e42c468613
Satya Chandu Dheeraj Balakavi <satyacha@buffalo.edu>
2015-03-29 19:42:35 -0400

    Update MyAvlTree.java

--- InProgress/MyAvlTree.java
+++ InProgress/MyAvlTree.java
@@ -32,24 +32,91 @@ public class MyAvlTree{
     public static Node insertNodeToTree (Node rootNode, int val) {
         if (rootNode != null) {
             if (val < rootNode.value) {
+                System.out.println ("Step-1: " + rootNode.value + "-" + val);
                 rootNode.leftChild = insertNodeToTree (rootNode.leftChild, val);
-                
-                if (rootNode.leftChild.leftChild != null) {
+                if (rootNode.leftChild.leftChild != null && rootNode.rightChild==null) {
+                    System.out.println ("11111");
                     Node grandPar = rootNode;
                     Node par = rootNode.leftChild;
                     Node child = rootNode.leftChild.leftChild;
-                    List <Node> list = new ArrayList <Node> (new Comparator<Node> () {
+                    List <Node> list = new ArrayList <Node> ();
+                    list.add (grandPar); list.add (par); list.add (child);
+                    Collections.sort (list, new Comparator<Node> () {
                         @Override
                         public int compare (Node n1, Node n2) {
-                            return n1.value.compare (n2.value);
+                            Integer i1 = n1.value;
+                            Integer i2 = n2.value;
+                            return i1.compareTo (i2);
                         }
                     });
                     rootNode = list.get (1);
                     rootNode.leftChild = list.get (0);
                     rootNode.rightChild = list.get (2);
+                    rootNode.leftChild.leftChild = null;
+                }
+                if (rootNode.leftChild.rightChild != null && rootNode.rightChild==null) {
+                    System.out.println ("22222");
+                    Node grandPar = rootNode;
+                    Node par = rootNode.leftChild;
+                    Node child = rootNode.leftChild.rightChild;
+                    List <Node> list = new ArrayList <Node> ();
+                    list.add (grandPar); list.add (par); list.add (child);
+                    Collections.sort (list, new Comparator<Node> () {
+                        @Override
+                        public int compare (Node n1, Node n2) {
+                            Integer i1 = n1.value;
+                            Integer i2 = n2.value;
+                            return i1.compareTo (i2);
+                        }
+                    });
+                    rootNode = list.get (1);
+                    rootNode.leftChild = list.get (0);
+                    rootNode.rightChild = list.get (2);
+                    rootNode.leftChild.rightChild = null;
                 } 
             } else if (val > rootNode.value) {
+                System.out.println ("Step-2: " + rootNode.value + "-" + val);
                 rootNode.rightChild = insertNodeToTree (rootNode.rightChild, val);
+                if (rootNode.rightChild.leftChild != null && rootNode.leftChild==null) {
+                    System.out.println ("33333");
+                    Node grandPar = rootNode;
+                    Node par = rootNode.rightChild;
+                    Node child = rootNode.rightChild.leftChild;
+                    List <Node> list = new ArrayList <Node> ();
+                    list.add (grandPar); list.add (par); list.add (child);
+                    Collections.sort (list, new Comparator<Node> () {
+                        @Override
+                        public int compare (Node n1, Node n2) {
+                            Integer i1 = n1.value;
+                            Integer i2 = n2.value;
+                            return i1.compareTo (i2);
+                        }
+                    });
+                    rootNode = list.get (1);
+                    rootNode.rightChild = list.get (0);
+                    rootNode.leftChild = list.get (2);
+                    rootNode.rightChild.leftChild = null;
+                } 
+                if (rootNode.rightChild.rightChild != null && rootNode.leftChild==null) {
+                    System.out.println ("44444");
+                    Node grandPar = rootNode;
+                    Node par = rootNode.rightChild;
+                    Node child = rootNode.rightChild.rightChild;
+                    List <Node> list = new ArrayList <Node> ();
+                    list.add (grandPar); list.add (par); list.add (child);
+                    Collections.sort (list, new Comparator<Node> () {
+                        @Override
+                        public int compare (Node n1, Node n2) {
+                            Integer i1 = n1.value;
+                            Integer i2 = n2.value;
+                            return i1.compareTo (i2);
+                        }
+                    });
+                    rootNode = list.get (1);
+                    rootNode.rightChild = list.get (0);
+                    rootNode.leftChild = list.get (2);
+                    rootNode.rightChild.rightChild = null;
+                } 
             } 
         }
         else {
